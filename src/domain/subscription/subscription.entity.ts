c6bc79a092a58c8023ab7aafc3391194e3df3b87
soo <dlgustn96@gmail.com>
2025-06-06 02:36:02 +0900

    refactor: SubscriptionEntity 생성자 필드 개별 인자로 변경

--- src/domain/subscription/subscription.entity.ts
+++ src/domain/subscription/subscription.entity.ts
@@ -1,26 +1,10 @@
-export type SubscriptionEntityProps = {
-  subscriptionId: number;
-  userId: number;
-  streamerId: number;
-  isConnected: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-};
-
 export class SubscriptionEntity {
-  public readonly subscriptionId: number;
-  public readonly userId: number;
-  public readonly streamerId: number;
-  public readonly isConnected: boolean;
-  public readonly createdAt: Date;
-  public readonly updatedAt: Date;
-
-  constructor(props: SubscriptionEntityProps) {
-    this.subscriptionId = props.subscriptionId;
-    this.userId = props.userId;
-    this.streamerId = props.streamerId;
-    this.isConnected = props.isConnected;
-    this.createdAt = props.createdAt;
-    this.updatedAt = props.updatedAt;
-  }
+  constructor(
+    public readonly subscriptionId: number,
+    public readonly userId: number,
+    public readonly streamerId: number,
+    public readonly isConnected: boolean,
+    public readonly createdAt: Date,
+    public readonly updatedAt: Date
+  ) {}
 }
