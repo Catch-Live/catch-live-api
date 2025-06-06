4c0e4624ad7ced52d67de8c3bb9eff965bc7433e
leesm0218 <leesm0218@naver.com>
2025-06-06 19:18:40 +0900

    test: db까지의 연결 테스트

--- test/interfaces/notification/notification.controller.spec.ts
+++ test/interfaces/notification/notification.controller.spec.ts
@@ -1,45 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { NotificationController } from 'src/interfaces/notification/notification.controller';
-import { NotificationUseCase } from 'src/application/notification/notification.use-case';
-
-const testData = [
-  {
-    notificationId: 1455,
-    content: "스트리머 '경제왕05'님의 라이브 녹화가 시작되었습니다.",
-    createdAt: '2025-03-21T10:10:10',
-  },
-  {
-    notificationId: 1456,
-    content: "스트리머 '경제왕06'님의 라이브 녹화가 시작되었습니다.",
-    createdAt: '2025-03-21T10:10:10',
-  },
-];
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { NotificationModule } from 'src/interfaces/notification/notification.module';
 
 describe('NotificationController', () => {
-  let controller: NotificationController;
+  let app: INestApplication;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [NotificationController],
-      providers: [
-        {
-          provide: NotificationUseCase,
-          useValue: {
-            getNotification: jest.fn().mockReturnValue(testData),
-          },
-        },
-      ],
+      imports: [NotificationModule],
     }).compile();
 
-    controller = module.get<NotificationController>(NotificationController);
+    app = module.createNestApplication();
+    await app.init();
   });
 
-  it('응답이 기대하던 형태로 전송되는지 확인', () => {
-    const response = controller.getNotification();
-    expect(response).toHaveProperty('code', '200');
-    expect(response).toHaveProperty('message', 'OK');
-    expect(response).toHaveProperty('data');
-    expect(response.data).toHaveProperty('notifications');
-    expect(response.data.notifications).toEqual(testData);
+  it('응답이 기대하던 형태로 전송되는지 확인', async () => {
+    const res = await request(app.getHttpServer()).get('/notifications');
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty('code', '200');
+    expect(res.body).toHaveProperty('message', 'OK');
+    expect(res.body).toHaveProperty('data');
+    expect(res.body.data).toHaveProperty('notifications');
+    expect(Array.isArray(res.body.data.notifications)).toBe(true);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
