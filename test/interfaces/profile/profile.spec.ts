6ce875c768be08ef15a36cfc19872bf0b6079aca
leesm0218 <leesm0218@naver.com>
2025-06-08 14:43:26 +0900

    test: db까지 연결하는 테스트코드 작성|

--- test/interfaces/profile/profile.spec.ts
+++ test/interfaces/profile/profile.spec.ts
@@ -1,35 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ProfileController } from 'src/interfaces/profile/profile.controller';
-import { ProfileUseCase } from 'src/application/profile/profile.use-case';
-
-const testData = {
-  provider: 'kakao',
-  email: 'kakao@kakao.com',
-  createdAt: new Date(),
-};
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { ProfileModule } from 'src/interfaces/profile/profile.module';
+import { ValidationPipe } from '@nestjs/common';
+import { $Enums } from '@prisma/client';
 
 describe('ProfileController', () => {
-  let controller: ProfileController;
+  let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [ProfileController],
-      providers: [ProfileUseCase],
+      imports: [ProfileModule],
     }).compile();
 
-    controller = module.get<ProfileController>(ProfileController);
+    app = module.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
+    await app.init();
   });
 
-  it('응답이 기대하던 형태로 전송되는지 확인', () => {
-    const response = controller.getProfile();
-    console.log(response);
-    expect(response).toHaveProperty('code', '200');
-    expect(response).toHaveProperty('message', 'OK');
-    expect(response).toHaveProperty('data');
-    expect(response.data).toHaveProperty('provider');
-    expect(response.data).toHaveProperty('email');
-    expect(response.data).toHaveProperty('createdAt');
-    expect(response.data.provider).toEqual(testData.provider);
-    expect(response.data.email).toEqual(testData.email);
+  it('db에 담긴 값이 오는지 확인', async () => {
+    const res = await request(app.getHttpServer()).get('/users/me');
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty('code', '200');
+    expect(res.body).toHaveProperty('message', 'OK');
+    expect(res.body).toHaveProperty('data');
+    expect(Object.values($Enums.Provider).includes(res.body.data.provider)).toBe(true);
+    expect(res.body.data).toHaveProperty('email', 'kakao@kakao.com');
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
