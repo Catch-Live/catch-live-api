07b3a108f6b4903d7775ad19f0a1bd2615c82f78
leesm0218 <leesm0218@naver.com>
2025-06-12 13:06:02 +0900

    fix: db 트랜잭션 오류 수정

--- src/infrastructure/logout/logout.core-repository.ts
+++ src/infrastructure/logout/logout.core-repository.ts
@@ -3,22 +3,26 @@ import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
 import { LogoutRequestDto } from 'src/interfaces/controller/logout/dto/logout.request.dto';
 import { LogoutRepository } from 'src/domain/logout/logout.repo';
 import { LogoutResponseResult } from 'src/domain/logout/result/logout.response.result';
+import { DomainCustomException } from 'src/domain/common/errors/domain-custom-exception';
+import { DomainErrorCode } from 'src/domain/common/errors/domain-error-code';
 
 @Injectable()
 export class LogoutCoreRepository implements LogoutRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async updateMany(requestDto: LogoutRequestDto) {
-    const rawData = await this.prisma.token.updateMany({
-      where: {
-        user_id: requestDto.userId,
-      },
-      data: { refresh_token: '', updated_at: new Date() },
-    });
+    await this.prisma.$transaction(async (prisma) => {
+      const rawData = await prisma.token.updateMany({
+        where: {
+          user_id: requestDto.userId,
+        },
+        data: { refresh_token: '', updated_at: new Date() },
+      });
 
-    if (rawData.count === 0 || rawData.count > 2) {
-      return new LogoutResponseResult(false);
-    }
+      if (rawData.count === 0 || rawData.count > 1) {
+        throw new DomainCustomException(500, DomainErrorCode.DB_SERVER_ERROR);
+      }
+    });
 
     return new LogoutResponseResult(true);
   }
