1c0014ad857bb8d708d7c6154b2c183ba13910eb
gt.park <smileboy0014@gmail.com>
2025-06-15 14:52:25 +0900

    feat: 트랜잭션 매니저 구현 및 글로벌 모듈 설정
    - 공통 인터페이스 TransactionManager 정의
    - Prisma 기반 트랜잭션 매니저 구현 (PrismaTransactionManager)
    - DI를 위한 토큰 TRANSACTION_MANAGER 등록
    - PrismaModule에 글로벌 설정 적용

--- src/infrastructure/prisma/prisma.module.ts
+++ src/infrastructure/prisma/prisma.module.ts
@@ -1,9 +1,17 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { PrismaTransactionManager } from './prisma.transaction-manager';
+import { TRANSACTION_MANAGER } from 'src/application/common/transaction-manager';
 
 @Global()
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: TRANSACTION_MANAGER,
+      useClass: PrismaTransactionManager,
+    },
+  ],
+  exports: [PrismaService, TRANSACTION_MANAGER],
 })
 export class PrismaModule {}
