b8fc1558b5f39a44258fa1e0bc19a9003058f8ee
leesm0218 <leesm0218@naver.com>
2025-06-17 16:02:58 +0900

    feat: 팀원과 상의하여 토큰 이름을 상수로 전환하고 쿠키 제거 기능 작성

--- src/interfaces/controller/user/user.controller.ts
+++ src/interfaces/controller/user/user.controller.ts
@@ -5,6 +5,7 @@ import { ResultResponseDto } from 'src/interfaces/controller/common/dto/result.r
 import { JwtAuthGuard } from 'src/interfaces/controller/common/guards/jwt-auth.guard';
 import { UserRequestDto } from 'src/interfaces/controller/user/dto/user.request.dto';
 import { UserResponseDto } from 'src/interfaces/controller/user/dto/user.response.dto';
+import { REFRESH_TOKEN_COOKIE_NAME } from 'src/support/constants';
 
 @UseGuards(JwtAuthGuard)
 @Controller('users')
@@ -15,6 +16,7 @@ export class UserController {
   async signoutUser(@Req() req: Request, @Res() res: Response) {
     const requestDto = new UserRequestDto(req);
     await this.userUseCase.signout(requestDto);
+    res.clearCookie(REFRESH_TOKEN_COOKIE_NAME, { path: '/', expires: new Date(0) });
     const data = new UserResponseDto(String(HttpStatus.OK), 'OK');
     res.status(HttpStatus.OK).json(ResultResponseDto.success(data));
   }
